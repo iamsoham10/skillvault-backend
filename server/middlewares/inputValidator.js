3784163a1d0cda6acafa0b2f15d78ca200ff4e23
Soham Chitale <chitalesoham106@gmail.com>
2025-01-24 06:37:03 +0530

    updated validators

--- server/middlewares/inputValidator.js
+++ server/middlewares/inputValidator.js
@@ -1,6 +1,6 @@
 const joi  = require('joi');
 
-const inputSchema = joi.object({
+const registerInputSchema = joi.object({
     username: joi.string()
         .alphanum()
         .min(3)
@@ -18,10 +18,19 @@ const paramSchema = joi.object({
     user_id: joi.string()
         .required()
         .pattern(new RegExp('^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$'))
+});
+
+const loginInputSchema = joi.object({
+    email: joi.string()
+        .email({minDomainSegments: 2})
+        .required(),
+    password: joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9!@#$%^&*()_+={};<>?,./-]{6,30}$'))
+        .required()
 })
 
 const checkInput = (req, res, next) => {
-    const {error} = inputSchema.validate(req.body);
+    const {error} = registerInputSchema.validate(req.body);
     if(error){
         return res.status(400).json({success: false, error: error.details[0].message})
     }
@@ -35,4 +44,12 @@ const checkParam = (req, res, next) => {
     }
     next();
 }
-module.exports = {checkInput, checkParam};
+
+const checkLoginInput = (req, res, next) => {
+    const {error} = loginInputSchema.validate(req.body);
+    if(error){
+        return res.status(400).json({success: false, error: error.details[0].message});
+    }
+    next();
+}
+module.exports = {checkInput, checkParam, checkLoginInput};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,7 +5,7 @@ const inputValidator = require("../middlewares/inputValidator");
 
 router.get("/user/:user_id", inputValidator.checkParam, authControllers.getUserController);
 router.post("/register", inputValidator.checkInput, authControllers.createUserController);
-router.post("/login", authControllers.loginUserController);
+router.post("/login", inputValidator.checkLoginInput, authControllers.loginUserController);
 router.post("/refresh-token", authControllers.refreshTokenController);
 router.put("/user/:id");
 router.delete("/user/:id");
