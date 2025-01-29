d681fcd52df40afc0d7f8ca96c2787bc254eeade
Soham Chitale <chitalesoham106@gmail.com>
2025-01-29 23:22:35 +0530

    created a resource model

---
+++ server/models/Resource.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const resourceSchema = new mongoose.Schema({
+    url: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+        validate: {
+            validator: function(v) {
+              return /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/.test(v);
+            },
+            message: props => `${props.value} is not a valid URL!`
+          }
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    description: {
+        type: String,
+    },
+    user_id: {
+        type: String,
+        required: true,
+    },
+    domain: {
+        type: String,
+        trim: true,
+    },
+    tags: [{
+        type: String,
+        trim: true,
+        index: true,
+    }],
+    favicon: {
+        type: String,
+        trim: true,
+    },
+    thumbnail: {
+        type: String,
+        trim: true
+    }
+}, {
+    strict: true
+}
+);
+
+const Resource = mongoose.model('Resource', resourceSchema);
+module.exports = Resource;
