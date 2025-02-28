5228a2d215012646c60e773a7df7bbaff127bc69
Soham Chitale <chitalesoham106@gmail.com>
2025-02-28 17:05:18 +0530

    primeNG & tailwind configured

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Dialog } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageService } from 'primeng/api';
+import {Toast} from 'primeng/toast';
+import { ToastModule } from 'primeng/toast';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  imports: [Dialog, ButtonModule, InputTextModule, Toast, ToastModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [MessageService, PrimeIcons]
 })
 export class AppComponent {
+  constructor(private messageService: MessageService) {}
   title = 'skillvault';
+  visible: boolean = false;
+
+  show() {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Info',
+      detail: 'Message Content',
+      life: 3000,
+    });
+  }
+  showDialog() {
+    this.visible = true;
+  }
+  toggleDarkMode() {
+    const element = document.querySelector('html');
+    if(element !== null){
+      element.classList.toggle('my-app-dark');
+    }
+}
 }
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Noir from '../../theme';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Noir,
+        options: {
+          darkModeSelector: '.my-app-dark'
+      }
+      }
+    }),
+  ],
 };
