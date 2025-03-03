4c9dafc71d74cdd3eadcba08b9105c13d096773c
Soham Chitale <chitalesoham106@gmail.com>
2025-03-04 00:04:21 +0530

    auth login service created

--- client/src/app/components/landing-page/auth-section/auth-section.component.ts
+++ client/src/app/components/landing-page/auth-section/auth-section.component.ts
@@ -1,17 +1,20 @@
+import {Component, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-auth-section',
-  imports: [DialogModule, ButtonModule, InputTextModule, CommonModule, PasswordModule],
+  imports: [DialogModule, ButtonModule, InputTextModule, CommonModule, PasswordModule, FormsModule, ReactiveFormsModule],
   templateUrl: './auth-section.component.html',
   styleUrl: './auth-section.component.css'
 })
 export class AuthSectionComponent {
+  value = ''
   visible = false;
   isLoginMode = true;
 
@@ -25,5 +28,27 @@ export class AuthSectionComponent {
 
   toggleMode(): void {
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  loginForm: FormGroup;
+  loginFormState = signal({email: '', password: ''});
+  constructor(private fb: FormBuilder, private authService: AuthService ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+    // subscribe to form changes and update the signal
+    this.loginForm.valueChanges.subscribe(value => {
+      this.loginFormState.set(value);
+    })
+  }
+  onSubmit(){
+    if(this.loginForm.valid){
+      this.authService.login(this.loginForm.value).subscribe(response => {
+        console.log(response);
+      }, error => {
+        console.log(error);
+      })
+    }
   }
 }
