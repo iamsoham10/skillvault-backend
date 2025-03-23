d147d5ce91d9d48b3db888ac78ae66c89f875516
Soham Chitale <chitalesoham106@gmail.com>
2025-03-23 23:06:15 +0530

    profile avatar added in navbar

--- client/src/app/shared/navbar/navbar.component.ts
+++ client/src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSignOut } from '@fortawesome/free-solid-svg-icons';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, AvatarModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   faSignOut = faSignOut;
+  userName = 'Soham Chitale';
 }
